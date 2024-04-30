381bbbf5d698d8c91c051b88ad79ea90da5c086f
Edward Solon <eddie@edwardsolon.com>
2024-04-29 22:33:11 -0500

    Core app added

--- webProject/urls.py
+++ webProject/urls.py
@@ -3,13 +3,19 @@ from django.conf.urls import include
 from django.urls import path
 # from django.views.generic import TemplateView
 from apps.boards import views as boards_views
+from apps.core import views as core_views
 
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.http import HttpResponse
+
+
 urlpatterns = [
     path("",boards_views.BoardListView.as_view(), name='home'),
-    # path("",TemplateView.as_view(template_name=""), name='home'),
+    path("core/",core_views.home, name='core'),
+    # path("demo/",doit(request):HttpResponse("MegaMiilion Demo Hello!")),
+    # path("core/",TemplateView.as_view(template_name=""), name='home'),
     path('boards/',include('apps.boards.urls')),
     path('accounts/',include('apps.accounts.urls')),    
     path('admin/', admin.site.urls),
